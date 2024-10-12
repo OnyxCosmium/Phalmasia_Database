76c5459ff8caa0b0e34dd10868730b62e46a56e1
OnyxCosmium <northern.aurora07@gmail.com>
2024-10-12 15:10:48 -0500

    Completed Transitioning Sigil Overviews

---
+++ Database Files/Magic/Sigils.md
@@ -0,0 +1,33 @@
+Sigils Overview (3 - 12)
+
+    Sigils are unique magic mechanics that are only available to those with two magics. Once a user unlocks their second element, a symbol shaped of either a circle, triangle, square, or diamond will manifest itself on the back, neck, chest, or stomach of the user. This sigil begins passively storing the user's mana, increasing the maximum amount the user can hold at once. It can hold up to 25% of the maximum mana capacity of the user.
+    
+    Though the user can use this stored mana normally, the user can tap into the power of the sigil, spreading the power across their body. This is shown by streams of mana extending over their limbs. This floods the body with the previously stored mana, increasing the strength, speed, and overall effectiveness of spells cast. However, this boost lasts proportionally long to the amount of mana stored, meaning those with higher amounts of stored mana can use sigil boosts for longer periods of time.
+    
+    If the user has a *mythical element*, however, this time limit is doubled, as they have heightened control over their mana, though it doesn't increase the power of *mythical elements* any.
+    
+    Sigils:
+    | Unity Sigils
+    | Sectional Sigils
+__________________________________________________________________________
+
+Unity Sigils (16 - 19)
+
+    Unity Sigils
+    
+    Unity Sigils are base sigils, and the ones that awaken for all their users. Unity sigils blend the colors of the two magics the user controls, then uses that swirled color to make up the sigil. The user of the sigil can choose to use the sigil's stored mana normally, or shoot the mana through the user all at once. If the user decides to do this, in a process dubbed a Mana Surge, the streams of mana that will cover the user's body will be composed of the two swirled colors of the sigil. This will boost the mana output and, because of mana's connection to life energy, the physical ability of the user by roughly 20%.
+__________________________________________________________________________
+
+Sectional Sigils (23 - 32)
+
+    Sectional Sigils
+    
+    Sectional Sigils, also known as Evolved Sigils, are attained by extreme mana control, and are completely up to the user's decision. A Unity Sigil will not evolve into a Sectional Sigil unless the user meets the requirements and wills the sigil to evolve. Once evolved into a Sectional Sigil, a user is completely unable to return to using a Unity Sigil.
+    
+    To attain a Sectional Sigil, the user must have mastered use of the Unity Sigil, and have completed an Energy Elevage. Once both of these requirements is met, the user must go through a unique version of Energy Elevage. Instead of focusing their mana into their own core, they will be forcing it into the sigil. This will cause it to destabilize and shut down, locking away sigil usage for one year. Unlike a usual Energy Elevage, however, this does not remove the ability to cast mana since it is taking place in the sigil and not the core. Once the year is up, the sigil reactivates and floods the body with the stored mana in what is essentially a forced Mana Surge that the user will not be able to turn off. After this is done, the process is fully complete, and the user will have a Sectional Sigil.
+    
+    The power of the Sectional Sigil is relatively similar in function to the Unity Sigil. However, instead of the blended color, the sigil sections the magic by evenly splitting itself, leaving each half of the sigil to reflect one of the user's magics. This means that the user is limited to using up to 50% of the sigil's stored energy for each magic. Each section stores 15% of the user's total mana capacity, for a total increased storage of 30%. This is due to the second Energy Elevage. 
+    
+    While activated completely, the user can choose to activate half of their sigil. When this is done, the streams that cover the user's body take on the color of the chosen magic. Because the user is only activating half of the sigil, the boost only lasts half as long as it did before. This only lasts for the chosen magic, meaning that the magic not associated with the activated half of the sigil will not get the power boost. Additionally, if both sides of the Sectional Sigil are used simultaneously, then the streams take on either one magic color of the two magic colors of the user, and the power boost given is raised to 50% for both magics. This boost cannot be turned off unlike a typical Mana Surge, and only stops once either half of the sigil runs out of magic. This boost is known as a Mana Baryon. Because the amount of mana flowing through the user was so much greater than the body is used to for such a long period of time, any magic that runs out of mana during a Mana Baryon cannot be used again until that magic's half of the sigil is completely filled again. This also means that the sigil must regenerate the lost mana on it's own over time; the user cannot simply refill the sigil manually. This may take anywhere from a day to a week depending on the mana capacity of the user's sigil.
+__________________________________________________________________________
+
--- Database Files/Phalmasia_Database.py
+++ Database Files/Phalmasia_Database.py
@@ -1763,90 +1763,59 @@ while infoBarrier == "" or infoBarrier == "return":
 
         # Sigils Commands
         while magicBarrier == "sigils":
-            sigilBarrier = input("Sigils:\n| About Sigils\n| Unity Sigils\n| Sectional Sigils\n\nLeave the database "
-                                 "by entering 'Leave'. Go back to magic selection prompt by entering "
-                                 "'Back'.\n").lower().strip()
+            # Sigils Overview
+            info_loc = []
+            for i in range(3, 12, 1): info_loc.append(i)
+            with open('Magic/Sigils.md', 'r') as file:
+                # Initialize a counter
+                line_number = 1
+
+                # Read and process each line
+                for line in file:
+                    if line_number in info_loc:
+                        print(f"{line.strip()}")
+                    line_number += 1
+
+            sigilBarrier = input("\nLeave the database by entering 'Leave'. Go back to magic selection prompt by entering 'Back'.\n").lower().strip()
 
             while sigilBarrier == "":
                 sigilBarrier = input("Please Re-enter your database restriction.\n").lower().strip()
-            while sigilBarrier != "unity sigils" and sigilBarrier != "sectional sigils" and sigilBarrier != "about sigils" and sigilBarrier != "sigils" and sigilBarrier != "unity" and sigilBarrier != "sectional" and sigilBarrier != "back" and sigilBarrier != "leave":
+            while sigilBarrier != "unity sigils" and sigilBarrier != "sectional sigils" and sigilBarrier != "unity" and sigilBarrier != "sectional" and sigilBarrier != "back" and sigilBarrier != "leave":
                 sigilBarrier = input("Please Re-enter your database restriction.\n").lower().strip()
 
-            # About Sigils Commands
-            if sigilBarrier == "about sigils" or sigilBarrier == "sigils":
-                sigilExplanation = "\033[1m" + "About Sigils" + "\033[0m\n\nSigils are unique magic mechanics that are only available to " \
-                                                                "those with two magics. Once a user unlocks their second element, a symbol shaped of " \
-                                                                "either a circle, triangle, square, or diamond will manifest itself on the back, " \
-                                                                "neck, chest, or stomach of the user. This sigil begins passively storing the " \
-                                                                "user's mana, increasing the maximum amount the user can hold at once. It can hold " \
-                                                                "up to 25% of the maximum mana capacity of the user.\n\nThough the user can use " \
-                                                                "this stored mana normally, the user can tap into the power of the " \
-                                                                "sigil, spreading the power across their body. This is shown by streams of mana " \
-                                                                "extending over their limbs. This floods the body with the previously stored mana, " \
-                                                                "increasing the strength, speed, and overall effectiveness of spells casted. However, this boost lasts proportionally " \
-                                                                "long to the amount of mana stored, meaning those with higher amounts of stored mana " \
-                                                                "can use sigil boosts for longer periods of time.\n\nIf the user has a *mythical " \
-                                                                "element*, however, this time limit is doubled, as they have heightened control over " \
-                                                                "their mana, though it doesn't increase the power of *mythical " \
-                                                                "elements* any."
-                sigils = input(sigilExplanation + "\n\nPress 'Enter' to return to magic selection.\n").lower().strip()
             # Unity Sigil Commands
             if sigilBarrier == "unity sigils" or sigilBarrier == "unity":
-                unityExplanation = (
-                            "\033[1m" + "Unity Sigils" + "\033[0m\n\nUnity Sigils are base sigils, and the ones that awaken for all their users. "
-                                                         "Unity sigils blend the colors of the two magics the user controls, then uses that "
-                                                         "swirled color to make up the sigil. The user of the sigil can choose to use the "
-                                                         "sigil's stored mana normally, or shoot the mana through the user all at once. If "
-                                                         "the user decides to do this, in a process dubbed a Mana Surge, the streams of mana "
-                                                         "that will cover the user's body will be composed of the two swirled colors of the "
-                                                         "sigil. This will boost the mana output and, because of mana's connection to life "
-                                                         "energy, the physical ability of the user by roughly 20%. ")
-                unitySigil = input(
-                    "Unity Sigils\n\n" + unityExplanation + "\n\nPress 'Enter' to return to magic selection.\n").lower().strip()
+                # Unity Sigils Overview
+                info_loc = []
+                for i in range(16, 19, 1): info_loc.append(i)
+                with open('Magic/Sigils.md', 'r') as file:
+                    # Initialize a counter
+                    line_number = 1
+
+                    # Read and process each line
+                    for line in file:
+                        if line_number in info_loc:
+                            print(f"{line.strip()}")
+                        line_number += 1
+
+                unitySigil = input("\nPress 'Enter' to return to magic selection.\n").lower().strip()
 
             # Sectional Sigil Commands
             if sigilBarrier == "sectional sigils" or sigilBarrier == "sectional":
-                sectionalExplanation = (
-                            "\033[1m" + "Sectional Sigils" + "\033[0m\n\nSectional Sigils, also known as Evolved Sigils, are attained by extreme mana "
-                                                             "control, and are completely up to the user's decision. A Unity Sigil will not "
-                                                             "evolve into a Sectional Sigil unless the user meets the requirements "
-                                                             "and wills the sigil to evolve. Once evolved into a Sectional Sigil, "
-                                                             "a user is completely unable to return to using a Unity Sigil.\n\nTo attain a "
-                                                             "Sectional Sigil, the user must have mastered use of the Unity Sigil, "
-                                                             "and have completed an Energy Elevage. Once both of these requirements is "
-                                                             "met, the user must go through a unique version of Energy Elevage. Instead of "
-                                                             "focusing their mana into their own core, they will be forcing it into the sigil. "
-                                                             "This will cause it to destabilize and shut down, locking away sigil usage for one year. "
-                                                             "Unlike a usual Energy Elevage, however, this does not remove the ability to cast "
-                                                             "mana since it is taking place in the sigil and not the core. Once the year is up, "
-                                                             "the sigil reactivates and floods the body with the stored mana in what is essentially "
-                                                             "a forced Mana Surge that the user will not be able to turn off. After this is done, "
-                                                             "the process is fully complete, and the user will have a Sectional Sigil.\n\nThe power of "
-                                                             "the Sectional Sigil is relatively similar in function "
-                                                             "to the Unity Sigil. However, instead of the blended color, the sigil sections "
-                                                             "the magic by evenly splitting itself, leaving each half of the sigil to "
-                                                             "reflect one of the user's magics. This means that the user is limited to "
-                                                             "using up to 50% of the sigil's stored energy for each magic. Each section "
-                                                             "stores 15% of the user's total mana capacity, for a total increased "
-                                                             "storage of 30%. This is due to the second Energy Elevage. \n\nWhile "
-                                                             "activated completely, the user can choose to activate half of their sigil. "
-                                                             "When this is done, the streams that cover the user's body take on the color "
-                                                             "of the chosen magic. Because the user is only activating half of the sigil, "
-                                                             "the boost only lasts half as long as it did before. This only lasts for the chosen magic, "
-                                                             "meaning that the magic not associated with the activated half of the sigil will not get the "
-                                                             "power boost. Additionally, if both sides of the Sectional Sigil are "
-                                                             "used simultaneously, then the streams take on either one magic color of "
-                                                             "the two magic colors of the user, and the power boost given is raised to 50% for "
-                                                             "both magics. This boost cannot be turned off unlike a typical Mana Surge, and only stops once either half of "
-                                                             "the sigil runs out of magic. This boost is known as a Mana Baryon. Because the amount of mana flowing through the user "
-                                                             "was so much greater than the body is used to for such a long period of time, any magic "
-                                                             "that runs out of mana during a Mana Baryon cannot be used again until that magic's half "
-                                                             "of the sigil is completely filled again. This also means that the sigil must regenerate "
-                                                             "the lost mana on it's own over time; the user cannot simply refill the sigil manually. "
-                                                             "This may take anywhere from a day to a week depending "
-                                                             "on the mana capacity of the user's sigil.")
-                sectionalSigil = input(
-                    "Sectional Sigils\n\n" + sectionalExplanation + "\n\nPress 'Enter' to return to magic selection.\n").lower().strip()
+                # Sectional Sigils Overview
+                info_loc = []
+                for i in range(23, 32, 1): info_loc.append(i)
+                with open('Magic/Sigils.md', 'r') as file:
+                    # Initialize a counter
+                    line_number = 1
+
+                    # Read and process each line
+                    for line in file:
+                        if line_number in info_loc:
+                            print(f"{line.strip()}")
+                        line_number += 1
+
+                sectionalSigil = input("\nPress 'Enter' to return to magic selection.\n").lower().strip()
 
             # Sigil Exit Command
             if sigilBarrier == "back":
